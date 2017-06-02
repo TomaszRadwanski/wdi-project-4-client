643a265ab7ec996eeb511135796c91aa206793f0
Tomasz Radwanski <t.l.radwanski@gmail.com>
2017-06-02 15:50:22 +0100

    angular authentication working

---
+++ src/js/config/router.config.js
@@ -0,0 +1,33 @@
+angular
+.module('AlcoholApp')
+.config(Router);
+
+Router.$inject = ['$stateProvider', '$locationProvider', '$urlRouterProvider'];
+function Router($stateProvider, $locationProvider, $urlRouterProvider){
+  $locationProvider.html5Mode(true);
+
+  $stateProvider
+  .state('home', {
+    url: '/',
+    templateUrl: '/js/views/home.html'
+  })
+  .state('userIndex', {
+    url: '/users',
+    templateUrl: '/js/views/users.html',
+    controller: 'UserCtrl',
+    controllerAs: 'user'
+  })
+  .state('register', {
+    url: '/register',
+    templateUrl: '/js/views/register.html',
+    controller: 'RegisterCtrl',
+    controllerAs: 'register'
+  })
+  .state('login', {
+    url: '/login',
+    templateUrl: '/js/views/login.html',
+    controller: 'LoginCtrl',
+    controllerAs: 'login'
+  });
+  $urlRouterProvider.otherwise('/');
+}
---
+++ src/js/constants/api.constant.js
@@ -0,0 +1,3 @@
+angular
+  .module('AlcoholApp')
+  .constant('API', `http://localhost:3000/api`);
